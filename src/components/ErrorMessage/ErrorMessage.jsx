822d7b4e1f31012ccbae2a200db9cb5f52804e6d
Alexandra0707 <alex07marta@gmail.com>
2024-12-25 13:28:41 +0000

    goit-react-hw-04

---
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,13 @@
+import s from "./ErrorMessage.module.css";
+
+const ErrorMessage = () => {
+  return (
+    <>
+      <p className={s.error}>
+        Sorry, something went wrong. Please try again later.
+      </p>
+    </>
+  );
+};
+
+export default ErrorMessage;
